3e312350c46970f7432fb8ad599633d992dbe6e9
Khubaib Farooq <khubaibf@usf.edu>
2019-09-12 23:06:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterative_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Please enter an number value which is between 1 and 100");
+                    int number = int.Parse(Console.ReadLine());
+
+                    if (number < 0 || number > 100)
+                    {
+                        Console.WriteLine("The number value should lie between 1 and 100");
+                    }
+                    else
+                    {
+                        for (int a = 1; a < 11; a++)
+                        {
+                            Console.WriteLine(" You have entered " + number + "This is the integer(number) value in the loop: " + a.ToString());
+                        }
+
+                    }
+                    Console.ReadKey();
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("For this program to run, please enter an integer(number) value");
+            }
+                
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Iterative Statement")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Iterative Statement")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("db59fdb8-7be4-47dc-bb72-e459cf5f42b2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
